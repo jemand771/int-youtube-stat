7699b361e3a69145984003c7af198a71e993bfda
jemand771 <jemand771@gmx.net>
2021-08-17 19:18:48 +0200

    add app stub

---
+++ youtube_api.py
@@ -0,0 +1,10 @@
+class YoutubeApi:
+
+    def __init__(self):
+        pass
+
+    def get_video_ids_from_playlist(self, playlist_id):
+        pass
+
+    def get_video_info(self, video_id):
+        pass
