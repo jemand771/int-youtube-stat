e17058fd3e356ea91d417fc911601426d9fdc814
Willy <jemand771@gmx.net>
2021-09-26 11:33:06 +0200

    PoC für fetch() und erste HTML-anfänge
    Co-authored-by: wildmoonraven <tessa-olbernhau@web.de>

---
+++ api_helper.py
@@ -0,0 +1,10 @@
+
+
+class YTApi:
+
+    def __init__(self):
+        pass
+
+
+class FakeYTApi(YTApi):
+    pass
--- app.py
+++ app.py
@@ -1,12 +1,34 @@
-from flask import Flask, render_template
+from flask import abort, Flask, jsonify, request, render_template, Response
 
-
-app = Flask(__name__)
+app: Flask = Flask(__name__)
 
 
 @app.get("/")
-def home_page():
+def home_page() -> str:
     return render_template("home.html")
+
+
+@app.get("/video_ids/<url>")
+def get_video_ids(url) -> Response:
+    print("fetching video ids for", url)
+    # TODO fetch video ids
+    return jsonify(["a", "b", "c"])
+
+
+@app.get("/video_info/<video_id>")
+def get_video_info(video_id: str) -> str:
+    print(video_id)
+    return ""
+
+
+# POST video_ids als json-array
+# z.B. ["foo", "bar", "asasdasdasd"]
+@app.post("/stats")
+def get_stats_from_video_ids() -> str:
+    if not isinstance(request.json, list):
+        abort(400, "request body needs to be of type list")
+    # TODO calculate stats
+    return ""
 
 
 if __name__ == "__main__":
