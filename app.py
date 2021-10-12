8a551fceb88d683aa067a0f809a1ed21f79b993e
Willy <jemand771@gmx.net>
2021-10-12 21:18:39 +0200

    api-zugriff auf url-parser und video-info-endpunkt

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import abort, Flask, jsonify, request, render_template, Response
 
+from api_helper import YouTubeApi
+
 app: Flask = Flask(__name__)
+api = YouTubeApi()
 
 
 @app.get("/")
@@ -8,17 +11,18 @@ def home_page() -> str:
     return render_template("home.html")
 
 
-@app.get("/video_ids/<url>")
+@app.get("/video_ids/<path:url>")
 def get_video_ids(url) -> Response:
+    # forward the query string from flask to the url parser inside the ytapi
+    if request.query_string:
+        url += "?" + request.query_string.decode("utf-8")
     print("fetching video ids for", url)
-    # TODO fetch video ids
-    return jsonify(["a", "b", "c"])
+    return jsonify(api.get_video_ids_from_link(url))
 
 
 @app.get("/video_info/<video_id>")
-def get_video_info(video_id: str) -> str:
-    print(video_id)
-    return ""
+def get_video_info(video_id: str):
+    return jsonify(api.get_video_data(video_id))
 
 
 # POST video_ids als json-array
