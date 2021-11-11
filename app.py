e24a4abd5eb03d9804693afa7e28fe66dab9aee8
Willy <jemand771@gmx.net>
2021-11-11 12:09:38 +0000

    granulare endpunkte eindampfen auf video_data
    ab jetzt nicht mehr erst ids aus playlist/anderer url anfragen und dann daten zu den ids
    sondern: direktes mapping url -> video-daten
    
    Co-authored-by: Colin P <admin@clnp.eu>

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import functools
 import os
+import re
 
 from flask import abort, Flask, jsonify, request, render_template, Response
 
@@ -26,24 +27,13 @@ def home_page() -> str:
     return render_template("home.html")
 
 
-@app.get("/video_ids/<path:url>")
-def get_video_ids(url) -> Response:
+@app.get("/video_data/<path:url>")
+def get_video_data_multi(url) -> Response:
     # forward the query string from flask to the url parser inside the ytapi
     if request.query_string:
         url += "?" + request.query_string.decode("utf-8")
-    print("fetching video ids for", url)
-    return jsonify(api.get_video_ids_from_link(url))
-
-
-@app.get("/video_info/<video_id>")
-def get_video_info(video_id: str) -> Response:
-    return jsonify(api.get_video_data(video_id))
-
-
-@validate_video_list
-@app.post("/video_info")
-def get_multi_video_info() -> Response:
-    return jsonify([api.get_video_data(video_id) for video_id in request.json])
+    print("fetching video data for", url)
+    return jsonify(api.get_video_data_multi(api.get_video_ids_from_link(url)))
 
 
 # POST video_ids als json-array
