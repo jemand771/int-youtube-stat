1873ffe74799e3f3a40818aa900e09f1cb96880c
wildmoonraven <tessa-olbernhau@web.de>
2021-11-20 11:30:44 +0100

    Testweise Videoobjekt erstellen + Formatierung by Franz

---
+++ scripts/Franz_script_zeug.js
@@ -0,0 +1,60 @@
+function createSingleVideoContainer(id, title, thumbnail, views, likes, length) {
+    let videoContainer = document.getElementsByClassName("scroll-box")[0];
+    let singleVideoContainer = document.createElement("section");
+    singleVideoContainer.setAttribute("class", "single-video-container");
+    singleVideoContainer.setAttribute("id", id);
+    videoContainer.appendChild(singleVideoContainer);
+
+    let singleVideoThumbnail = document.createElement("div");
+    singleVideoThumbnail.setAttribute("class", "single-video-thumbnail");
+    singleVideoThumbnail.setAttribute("id", id);
+    let thumbnailCanvas = function () {
+        let canvas = document.createElement("canvas");
+        canvas.setAttribute("id", id);
+        canvas.setAttribute("width", "200");
+        canvas.setAttribute("height", "100");
+        //TODO put thumbnail here
+        let ctx = canvas.getContext("2d");
+        ctx.moveTo(0, 0);
+        ctx.lineTo(200, 100);
+        ctx.moveTo(200, 0);
+        ctx.lineTo(0, 100)
+        ctx.stroke();
+        ctx.arc(100, 50, 50, 0, 2 * Math.PI);
+        ctx.stroke();
+        return canvas;
+    };
+    console.log(thumbnailCanvas());
+    singleVideoThumbnail.appendChild(thumbnailCanvas());
+    singleVideoContainer.appendChild(singleVideoThumbnail);
+
+    let singleVideoStats = document.createElement("div");
+    singleVideoStats.setAttribute("class", "single-video-stats");
+    singleVideoStats.setAttribute("id", id);
+    let videoTitle = document.createElement("h3");
+    videoTitle.innerHTML = title;
+    singleVideoStats.appendChild(videoTitle);
+    let videoLikes = document.createElement("p");
+    videoLikes.innerHTML = "Bewertung: " + likes;
+    singleVideoStats.appendChild(videoLikes);
+    let videoViews = document.createElement("p");
+    videoViews.innerHTML = "Views: " + views;
+    singleVideoStats.appendChild(videoViews);
+    let videoLength = document.createElement("p");
+    videoLength.innerHTML = "Länge: " + length;
+    singleVideoStats.appendChild(videoLength);
+    singleVideoContainer.appendChild(singleVideoStats);
+    let delBut = document.createElement("button");
+    delBut.setAttribute("class", "btn-delete-element");
+    delBut.setAttribute("onClick", "removeChildElement(this)");
+    let deleteIcon = document.createElement("span");
+    deleteIcon.setAttribute("class", "material-icons");
+    deleteIcon.innerHTML = "delete";
+    delBut.appendChild(deleteIcon);
+    singleVideoContainer.appendChild(delBut);
+
+}
+
+function removeChildElement(childElement) {
+    childElement.parentElement.remove();
+}
