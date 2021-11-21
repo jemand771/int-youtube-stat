f54a9e995b964962f0a6398d58a8a580a8d1ad7b
Colin Pohle <colin.pohle@gmail.com>
2021-11-21 19:02:28 +0100

    Seriösere Fehlermeldung

--- api_helper.py
+++ api_helper.py
@@ -9,8 +9,8 @@ import pyyoutube
 
 
 class InvalidLinkFormatException(ValueError):
-    def __init__(self, message):
-        super().__init__(f"invalid link: {message}")
+    def __init__(self):
+        super().__init__(f"invalid link")
 
 
 class VideoNotFoundException(ValueError):
@@ -93,7 +93,7 @@ class YouTubeApi:
                     return [query.get("v")]
         elif url_obj.netloc == "youtu.be":
             return [url_obj.path[1:]]
-        raise InvalidLinkFormatException("meh")
+        raise InvalidLinkFormatException()
 
     @cached
     def get_video_data(self, video_id: str) -> YouTubeVideo:
