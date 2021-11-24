355fb0c9039f6409b18f0ec2cefb408608969bbf
Hallopfosten <franzschaffh@aol.de>
2021-11-24 19:56:20 +0100

    fixed fehlende stats-aktualisierung, wenn letztes Video manuell gelöscht wurde

--- static/js/updateStatistics.js
+++ static/js/updateStatistics.js
@@ -1,30 +1,29 @@
     function updateStatistics() {
         let videos = document.getElementsByClassName("single-video-container");
-        // console.log(videos);
-        let ids = new Array();
-        for (let i = 0; i < videos.length; i++) {
-            ids[i] = videos[i].getAttribute("id");
+        if (videos.length > 0) {
+            let ids = new Array();
+            for (let i = 0; i < videos.length; i++) {
+                ids[i] = videos[i].getAttribute("id");
+            }
+            fetch("/stats", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(ids),
+            })
+            .then(res => res.json())
+            .then((out) => {
+                document.getElementById("label-video-count").innerHTML = videos.length;
+                document.getElementById("label-combined-duration").innerHTML = out.total_duration;
+                document.getElementById("label-average-duration").innerHTML = out.avg_duration;
+                document.getElementById("label-average-likes").innerHTML = out.avg_likes;
+                document.getElementById("label-average-views").innerHTML = out.avg_views;
+            }).catch(err => {
+            console.error(err);
+            });
         }
-        // console.log(ids);
-        fetch("/stats", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-//        json: ids,
-        body: JSON.stringify(ids),
-        })
-        .then(res => res.json())
-        .then((out) => {
-            document.getElementById("label-video-count").innerHTML = videos.length;
-            document.getElementById("label-combined-duration").innerHTML = out.total_duration;
-            document.getElementById("label-average-duration").innerHTML = out.avg_duration;
-            document.getElementById("label-average-likes").innerHTML = out.avg_likes;
-            document.getElementById("label-average-views").innerHTML = out.avg_views;
-        }).catch(err => {
-        console.error(err);
-        });
-        }
+    }
 
 
     function resetStatistics() {
