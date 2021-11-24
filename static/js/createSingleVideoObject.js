36a6a546466f56ab7e64c322807d2ad0b45c012a
Hallopfosten <franzschaffh@aol.de>
2021-11-24 19:52:43 +0100

    function um einzelne Video-Objekte zu löschen in andere Datei verschoben und umbenannt, weil's da mehr sinn macht

--- static/js/createSingleVideoObject.js
+++ static/js/createSingleVideoObject.js
@@ -59,8 +59,3 @@ function createSingleVideoContainer(id, title, thumbnail, views, likes, duration
     singleVideoContainer.appendChild(delBut);
 
 }
-//TODO add delete all elements button
-function removeChildElement(childElement) {
-    childElement.parentElement.remove();
-    updateStatistics()
-}
--- static/js/deleteAllVideoObjects.js
+++
@@ -1,5 +0,0 @@
-    function deleteAllElements() {
-        let parent = document.getElementsByClassName("scroll-box")[0];
-        while (parent.firstChild) parent.removeChild(parent.firstChild);
-        resetStatistics();
-    }
